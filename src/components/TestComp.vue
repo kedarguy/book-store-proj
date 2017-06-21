b1a441e290da3dc3934bc3b9da5962621bba8657
kedarguy <kedarguy@gmail.com>
2017-06-21 09:49:54 +0300

    first branch test commit

---
+++ src/components/TestComp.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="popup book-details">
+    testing 123
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: ['book'],
+  methods: {
+    closeMe() {
+      console.log('close me!');
+      this.$emit('close');
+    },
+    requestNextBook() {
+      console.log('Requesting Next');
+      this.$emit('next');
+    }
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
